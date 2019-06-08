c94af443eddb9de7282623133a548c9cdca49436
gayathri0609 <gayathrid6995@gmail.com>
2019-06-08 11:32:02 +0530

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+import { CreateDoctorComponent } from './create-doctor/create-doctor.component';
+import { DoctorListComponent } from './doctor-list/doctor-list.component';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, CreateDoctorComponent, DoctorListComponent, HeaderNavComponent, LeftNavComponent, LoginComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
